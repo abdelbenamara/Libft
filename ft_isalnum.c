9c8dbe99d591ee672c3bdfb07f599cebfac104c0
abdelbenamara <abdelb.contact@gmail.com>
2022-11-12 01:11:06 +0000

    Added ft_isalnum

---
+++ ft_isalnum.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 00:54:40 by abenamar          #+#    #+#             */
+/*   Updated: 2022/11/12 00:58:14 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	ft_isalpha(int c)
+{
+	return (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'));
+}
+
+static int	ft_isdigit(int c)
+{
+	return ('0' <= c && c <= '9');
+}
+
+int ft_isalnum(int c)
+{
+	return (ft_isalpha(c) || ft_isdigit(c));
+}
