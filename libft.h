02e917857a31f3bf5d7ae3b3b5bb57816a1e3cf4
abdelbenamara <abdelb.contact@gmail.com>
2022-11-12 01:11:46 +0000

    Updated libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 17:27:57 by abenamar          #+#    #+#             */
-/*   Updated: 2022/11/11 17:28:00 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/11/12 01:00:19 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
+int	ft_isalnum(int c);
 
 #endif
