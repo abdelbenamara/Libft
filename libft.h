1e783ec1b5bbd452b7c52a52d65a4d1308d4e4b4
abdelbenamara <abdelb.contact@gmail.com>
2022-11-13 03:54:05 +0000

    Updated libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 17:27:57 by abenamar          #+#    #+#             */
-/*   Updated: 2022/11/13 03:01:30 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/11/13 03:32:05 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 
 #endif
