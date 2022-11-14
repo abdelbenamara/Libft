bc3793bf495db921e67eb6e5ff30422c3da7c593
abdelbenamara <abdelb.contact@gmail.com>
2022-11-14 00:51:23 +0000

    Updated ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 01:20:52 by abenamar          #+#    #+#             */
-/*   Updated: 2022/11/13 02:56:04 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/11/14 00:45:01 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	*ft_memset(void *s, int c, size_t n)
 	void	*p;
 
 	p = s;
-	while (n-- > 0)
-		*(uint8_t *)s++ = (uint8_t)c;
+	if (s != NULL)
+		while (n-- > 0)
+			*(uint8_t *)s++ = (uint8_t)c;
 	return (p);
 }
