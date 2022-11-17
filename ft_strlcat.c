b72f861ba4354555ed6acc3af6450b2d2eebf5b3
abdelbenamara <abdelb.contact@gmail.com>
2022-11-17 20:50:01 +0000

    Updated ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 22:33:58 by abenamar          #+#    #+#             */
-/*   Updated: 2022/11/17 09:58:05 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/11/17 18:29:05 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,16 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t		dst_len;
-	size_t		src_len;
+	char		*d;
+	const char	*s;
 
 	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	if (size <= dst_len)
-		return (size + src_len);
-	if (src_len < size - dst_len)
-		ft_strlcpy(dst + dst_len, src, src_len + 1);
-	else
-		ft_strlcpy(dst + dst_len, src, size - dst_len);
-	return (dst_len + src_len);
+	if (!dst || !src || size <= dst_len)
+		return (size + ft_strlen(src));
+	d = dst + dst_len;
+	s = src;
+	while (--size > dst_len && *s)
+		*d++ = *s++;
+	*d = '\0';
+	return (dst_len + ft_strlen(src));
 }
