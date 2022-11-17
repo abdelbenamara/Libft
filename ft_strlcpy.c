a529b7b0bae452ddf611971f56dbfb92f80c694a
abdelbenamara <abdelb.contact@gmail.com>
2022-11-17 03:19:41 +0000

    Upated ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 00:18:54 by abenamar          #+#    #+#             */
-/*   Updated: 2022/11/16 22:43:57 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/11/17 02:52:11 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t	src_len;
 	size_t	dst_len;
+	size_t	src_len;
 
-	src_len = ft_strlen(src);
+	if (!size)
+		return (ft_strlen(src));
 	dst_len = size - 1;
+	src_len = ft_strlen(src);
 	if (dst_len > src_len)
 		dst_len = src_len;
-	if (dst != NULL && src != NULL && size > 0)
-	{
+	if (dst && src)
 		ft_memcpy(dst, src, dst_len);
-		dst[dst_len] = '\0';
-	}
+	dst[dst_len] = '\0';
 	return (src_len);
 }
