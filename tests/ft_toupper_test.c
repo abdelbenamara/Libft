e309a4b373e9c023679858150d68d2600657ef5b
abdelbenamara <abdelb.contact@gmail.com>
2022-11-18 21:24:39 +0000

    Updated tests

---
+++ tests/ft_toupper_test.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper_test.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 21:02:50 by abenamar          #+#    #+#             */
+/*   Updated: 2022/11/18 21:16:10 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
+#include "libft.h"
+
+#define RESET   	"\033[0m"
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+
+void	ft_toupper_test(void)
+{
+	printf("ft_toupper\t\t\t");
+	assert(ft_toupper('A') == toupper('A'));
+	printf(BOLDGREEN "1. OK\t");
+	assert(ft_toupper('Z') == toupper('Z'));
+	printf(BOLDGREEN "2. OK\t");
+	assert(ft_toupper('M') == toupper('M'));
+	printf(BOLDGREEN "3. OK\t");
+	assert(ft_toupper('a') == toupper('a'));
+	printf(BOLDGREEN "4. OK\t");
+	assert(ft_toupper('z') == toupper('z'));
+	printf(BOLDGREEN "5. OK\t");
+	assert(ft_toupper('m') == toupper('m'));
+	printf(BOLDGREEN "6. OK\t");
+	assert(ft_toupper('\0') == toupper('\0'));
+	printf(BOLDGREEN "7. OK\t");
+	assert(ft_toupper('1') == toupper('1'));
+	printf(BOLDGREEN "8. OK\t");
+	assert(ft_toupper(224) == toupper(224));
+	printf(BOLDGREEN "9. OK\t");
+	assert(ft_toupper(' ') == toupper(' '));
+	printf(BOLDGREEN "10. OK" RESET "\n");
+}
--- tests/test.c
+++ tests/test.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 15:13:18 by abenamar          #+#    #+#             */
-/*   Updated: 2022/11/17 03:18:08 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/11/18 21:05:43 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void	ft_memcpy_test(void);
 void	ft_memmove_test(void);
 void	ft_strlcpy_test(void);
 void	ft_strlcat_test(void);
+void	ft_toupper_test(void);
 
 int	main(void)
 {
@@ -46,5 +47,6 @@ int	main(void)
 	ft_memmove_test();
 	ft_strlcpy_test();
 	ft_strlcat_test();
+	ft_toupper_test();
 	return (0);
 }
