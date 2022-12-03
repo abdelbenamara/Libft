f2d103e0b16d1ac92f3e2390c12c8631bad8325a
abdelbenamara <abdelb.contact@gmail.com>
2022-12-03 01:27:28 +0000

    Updated ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 21:55:39 by abenamar          #+#    #+#             */
-/*   Updated: 2022/11/22 21:20:46 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/12/03 00:19:19 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	char			*r;
-	unsigned char	b;
+	const unsigned char	b = c;
+	char				*r;
 
 	r = NULL;
 	if (!s)
 		return (r);
-	b = c;
 	while (*s)
 	{
 		if (*s == b)
