9edb24dc58927fc94b7bee889a874b38c3463220
abdelbenamara <abdelb.contact@gmail.com>
2022-12-03 01:26:35 +0000

    Updated ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 23:22:31 by abenamar          #+#    #+#             */
-/*   Updated: 2022/11/17 18:24:25 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/12/03 00:04:27 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
+	const uint8_t	*s = src;
 	uint8_t			*d;
-	const uint8_t	*s;
 
 	if (!dest || !src || !n)
 		return (dest);
 	d = dest;
-	s = src;
 	while (n--)
 		*d++ = *s++;
 	return (dest);
