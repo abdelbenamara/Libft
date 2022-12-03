c8b3a9fb9b954b06bb0e1a3ac93902aa081f0e7c
abdelbenamara <abdelb.contact@gmail.com>
2022-12-03 01:28:09 +0000

    Updated ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 21:44:40 by abenamar          #+#    #+#             */
-/*   Updated: 2022/11/24 23:30:04 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/12/03 00:14:00 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	big_len;
-	size_t	little_len;
+	const size_t	little_len = ft_strlen(little);
+	size_t			big_len;
 
 	if (!big || !little || !len)
 		return (NULL);
 	else if (*little == '\0')
 		return ((char *) big);
 	big_len = ft_strlen(big);
-	little_len = ft_strlen(little);
 	while (little_len <= big_len && little_len <= len)
 	{
 		if (!ft_strncmp(big, little, little_len))
