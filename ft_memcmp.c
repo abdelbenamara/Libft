1a98b826072b25a087fe10bfb18937fbe8436a49
abdelbenamara <abdelb.contact@gmail.com>
2022-12-03 01:28:02 +0000

    Updated ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 22:09:46 by abenamar          #+#    #+#             */
-/*   Updated: 2022/11/23 22:29:30 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/12/03 00:06:10 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*r1;
-	unsigned char	*r2;
+	const unsigned char	*r1 = (unsigned char *) s1;
+	const unsigned char	*r2 = (unsigned char *) s2;
 
 	if (!s1 && s2)
 		return (*(unsigned char *)s2);
@@ -23,8 +23,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 		return (*(unsigned char *)s1);
 	else if (!s1 || !s2 || !n)
 		return (0);
-	r1 = (unsigned char *) s1;
-	r2 = (unsigned char *) s2;
 	while (--n)
 	{
 		if (*r1 != *r2)
