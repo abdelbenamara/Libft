6d5d3809bd136643b909e0788a8f70d56619c590
abdelbenamara <abdelb.contact@gmail.com>
2022-12-04 14:57:07 +0000

    Added ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 12:55:42 by abenamar          #+#    #+#             */
+/*   Updated: 2022/12/04 14:43:40 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	const size_t	len = ft_strlen(s1) + ft_strlen(s2);
+	char			*join;
+
+	join = ft_calloc(len + 1, sizeof(char));
+	if (!join)
+		return (NULL);
+	ft_strlcpy(join, s1, len + 1);
+	ft_strlcat(join, s2, len + 1);
+	return (join);
+}
