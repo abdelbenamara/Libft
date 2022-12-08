a44a7bb6aa2acad4d71ebc530305e59a46a14a29
abdelbenamara <abdelb.contact@gmail.com>
2022-12-08 20:41:20 +0000

    Updated ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 21:36:07 by abenamar          #+#    #+#             */
-/*   Updated: 2022/11/22 22:35:37 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/12/08 18:36:13 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,21 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	const unsigned char	*r1 = (unsigned char *) s1;
+	const unsigned char	*r2 = (unsigned char *) s2;
+
 	if (!s1 && s2)
-		return (*s2);
+		return (0 - *r2);
 	else if (s1 && !s2)
-		return (*s1);
+		return (*r1);
 	else if (!s1 || !s2 || !n)
 		return (0);
-	while (--n && *s1 && *s2)
+	while (--n && *r1 && *r2)
 	{
-		if (*s1 != *s2)
+		if (*r1 != *r2)
 			break ;
-		++s1;
-		++s2;
+		++r1;
+		++r2;
 	}
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	return (*r1 - *r2);
 }
