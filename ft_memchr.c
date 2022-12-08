94cff27df8a4e678f5a1eb1c881a6303d035b9a8
abdelbenamara <abdelb.contact@gmail.com>
2022-12-08 20:41:29 +0000

    Updated ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 00:06:43 by abenamar          #+#    #+#             */
-/*   Updated: 2022/12/03 00:09:16 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/12/08 20:07:51 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,18 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const uint8_t	b = c;
-	uint8_t			*r;
+	const unsigned char	b = c;
+	const unsigned char	*r = s;
 
 	if (!s || !n)
 		return (NULL);
-	r = (uint8_t *) s;
-	while (--n && *r)
+	while (--n)
 	{
 		if (*r == b)
 			break ;
 		++r;
 	}
 	if (*r == b)
-		return (r);
+		return ((unsigned char *) r);
 	return (NULL);
 }
