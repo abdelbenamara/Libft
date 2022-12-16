b02b83928caaf13ad108ac1f08e22d99046d785b
abdelbenamara <abdelb.contact@gmail.com>
2022-12-16 03:18:18 +0000

    Added ft_lstsize_bonus

---
+++ ft_lstsize_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/16 03:00:55 by abenamar          #+#    #+#             */
+/*   Updated: 2022/12/16 03:06:56 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	size;
+
+	if (!lst)
+		return (0);
+	size = 1;
+	while (lst->next)
+	{
+		++size;
+		lst = lst->next;
+	}
+	return (size);
+}
