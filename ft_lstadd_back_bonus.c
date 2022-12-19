a27cce4ac2958051ffb81010cf7599b0db8e233e
abdelbenamara <abdelb.contact@gmail.com>
2022-12-19 20:21:21 +0000

    Added ft_lstadd_back_bonus

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 19:51:09 by abenamar          #+#    #+#             */
+/*   Updated: 2022/12/19 20:18:52 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*item;
+
+	if (!lst || !new)
+		return ;
+	item = *lst;
+	while (item->next)
+		item = item->next;
+	item->next = new;
+}
