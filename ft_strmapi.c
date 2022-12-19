4fa30ff9e606120d2779d27e256f471b6db3e972
abdelbenamara <abdelb.contact@gmail.com>
2022-12-19 21:34:33 +0000

    Updated ft_strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 23:15:24 by abenamar          #+#    #+#             */
-/*   Updated: 2022/12/08 23:37:48 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/12/19 20:41:30 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	unsigned int	i;
 	char			*r;
 
+	if (!s || !f)
+		return (NULL);
 	i = 0;
 	r = ft_strdup(s);
 	if (!r)
