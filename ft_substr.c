565664efbb4c4092b2490c83a1fb8b2f0d9e8062
abdelbenamara <abdelb.contact@gmail.com>
2022-12-22 21:56:45 +0000

    Updated ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 11:45:13 by abenamar          #+#    #+#             */
-/*   Updated: 2022/12/04 12:41:58 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/12/22 19:01:06 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	sub_len;
 	char	*sub;
 
-	sub_len = ft_strlen(s + start);
+	if (!s)
+		return (NULL);
+	sub_len = ft_strlen(s);
+	if (start > sub_len)
+		sub_len = 0;
+	else
+		sub_len = sub_len - start;
 	if (len < sub_len)
 		sub_len = len;
 	sub = ft_calloc(sub_len + 1, sizeof(char));
