8600c9634af24abc8832440954785222799150fc
abdelbenamara <abdelb.contact@gmail.com>
2022-12-22 21:54:13 +0000

    Updated ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 00:18:54 by abenamar          #+#    #+#             */
-/*   Updated: 2022/12/08 18:21:52 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/12/22 17:59:15 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	const char	*s = src;
 
-	if (!dst || !src || !size)
+	if (!dst || !size)
 		return (ft_strlen(src));
 	while (--size && *s)
-		*dst++ = *s++;
+	{
+		*dst = *s;
+		++dst;
+		++s;
+	}
 	*dst = '\0';
 	while (*s)
 		++s;
