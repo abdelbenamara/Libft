747a6311173428e5043dd5132d3049191c876d61
abdelbenamara <abdelb.contact@gmail.com>
2022-12-22 21:56:26 +0000

    Updated ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 23:38:33 by abenamar          #+#    #+#             */
-/*   Updated: 2022/12/08 21:53:34 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/12/22 17:28:17 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,20 @@ int	ft_atoi(const char *nptr)
 	int	i;
 	int	sign;
 
-	if (!nptr)
-		return (0);
 	while (*nptr == ' ' || (9 <= *nptr && *nptr <= 13))
 		++nptr;
 	i = 0;
 	sign = 1;
 	if (*nptr == '+' || *nptr == '-')
-		if (*nptr++ == '-')
+	{
+		if (*nptr == '-')
 			sign = -1;
+		++nptr;
+	}
 	while (ft_isdigit(*nptr))
-		i = i * 10 + (*nptr++ - '0');
+	{
+		i = i * 10 + (*nptr - '0');
+		++nptr;
+	}
 	return (sign * i);
 }
