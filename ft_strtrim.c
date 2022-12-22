e57e261486025f8864d7e0a5435197d9c1a08c2f
abdelbenamara <abdelb.contact@gmail.com>
2022-12-22 21:57:51 +0000

    Updated ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,28 +6,49 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 15:04:09 by abenamar          #+#    #+#             */
-/*   Updated: 2022/12/05 15:05:32 by abenamar         ###   ########.fr       */
+/*   Updated: 2022/12/22 19:35:41 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	is_char_in_set(char c, char const *set)
+{
+	if (!set)
+		return (0);
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		++set;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	const size_t	set_len = ft_strlen(set);
-	size_t			len;
-	char			*str;
+	size_t	len;
+	char	*trim;
 
-	if (set_len)
-		while (!ft_strncmp(s1, set, set_len))
-			s1 += set_len;
-	len = ft_strlen(s1);
-	if (set_len)
-		while (!ft_strncmp(s1 + len - set_len, set, set_len))
-			len -= set_len;
-	str = ft_calloc(len + 1, sizeof(char));
-	if (!str)
+	if (!s1)
 		return (NULL);
-	ft_strlcpy(str, s1, len + 1);
-	return (str);
+	len = 0;
+	while (*s1)
+	{
+		if (!is_char_in_set(*s1, set))
+			break ;
+		++s1;
+	}
+	len = ft_strlen(s1);
+	while (len)
+	{
+		if (!is_char_in_set(*(s1 + len - 1), set))
+			break ;
+		--len;
+	}
+	trim = ft_calloc(len + 1, sizeof(char));
+	if (!trim)
+		return (NULL);
+	ft_strlcpy(trim, s1, len + 1);
+	return (trim);
 }
